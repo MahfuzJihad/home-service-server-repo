024cf060262c48256c6824d9cd84299a72ad4c75
jihadmahfuz <jihadmah166@gmail.com>
2022-11-15 13:02:13 +0600

    seven commit

--- db/conn.js
+++ db/conn.js
@@ -6,23 +6,23 @@ const client = new MongoClient(connectionString, {
   useUnifiedTopology: true,
 });
 
-let dbConnection;
+// let dbConnection;
 
-module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      if (err || !db) {
-        return callback(err);
-      }
+// module.exports = {
+//   connectToServer: function (callback) {
+//     client.connect(function (err, db) {
+//       if (err || !db) {
+//         return callback(err);
+//       }
 
-      dbConnection = db.db('homeservice');
-      console.log('Successfully connected to MongoDB.');
+//       dbConnection = db.db('homeservice');
+//       console.log('Successfully connected to MongoDB.');
 
-      return callback();
-    });
-  },
+//       return callback();
+//     });
+//   },
 
-  getDb: function () {
-    return dbConnection;
-  },
-};
+//   getDb: function () {
+//     return dbConnection;
+//   },
+// };
